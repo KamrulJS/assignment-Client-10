2b23d1ed4fda8211ac0e9b342b02732d3ca47168
KamrulJS <mkkamrulislampk@gmail.com>
2023-08-09 00:20:06 +0600

    6th

--- src/component/Error.jsx
+++ src/component/Error.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import img from "../images/error.jpg"
 
+
 const Error = () => {
-    
+
     return (
         <div className='w-2/5 h-2/5 mx-auto'>
             <img src={img} alt=""/>
