345692855fd01daca006540728c4aec093c113c2
KamrulJS <mkkamrulislampk@gmail.com>
2023-08-09 00:21:07 +0600

    9thh

--- src/component/Chefs.jsx
+++ src/component/Chefs.jsx
@@ -2,8 +2,10 @@ import React, { useContext } from 'react';
 import AuthProvider, { AuthContext } from '../provider/AuthProvider';
 import Chef from './chef/Chef';
 
-const Chefs = ({children}) => {
-    const {datas}=useContext(AuthContext);
+const Chefs = ({ children }) => {
+    
+    const { datas } = useContext(AuthContext);
+    
     return (
         <div className='w-4/5 mx-auto mt-10'>
             <h1 className='title my-10'>Our Chefs</h1>
